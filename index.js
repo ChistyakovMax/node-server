e9be8dbf6d3baef4b14abd9bd547fdac499abd1b
just_a_max <chistyakovmaxim2000@gmail.com>
2023-10-14 19:18:35 +0300

    task3 done

--- index.js
+++ index.js
@@ -1,39 +1,74 @@
 import x from "express";
+import {fileURLToPath} from "url";
 
+const CORS = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,POST,DELETE",
+    "Access-Control-Allow-Headers": "Access-Control-Allow-Headers"
+};
+const setContentTypeJson = {
+    "Content-Type": "application/json; charset=utf-8"
+};
+const setContentTypeText = {
+    "Content-type": "text/plain; charset=UTF-8"
+};
+const setContentTypeHTML = {
+    "Content-type": "text/html; charset=UTF-8"
+};
 const app = x();
-app.get("/", (req, res) => {
+
+app
+.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  })
+.get("/", (req, res) => {
     res
     .status(666)
-    .set({ "Content-Type": "application/json; charset=utf-8" })
+    .set(setContentTypeJson)
     .json('{"Hello": "world"}');
-});
-
-app.all("/login/", (req, res) => {
+})
+//Task1
+.all("/login/", (req, res) => {
     try {
         res
-        .setHeader("Content-Type", "text/plain; charset=UTF-8")
-        .setHeader("Access-Control-Allow-Origin", "*")
-        .end("itmo370137");
+        .send("itmo370137");
     } catch (error) {
         console.error("Произошла ошибка:", error);
         res.status(600).send("Произошла ошибка");
     }
-});
-
-const func = "function task(x) {\n\treturn x * Math.pow(x,2);\n}";
-
-app.all("/sample/", (req, res) => {
+})
+//Task2
+.all("/sample/", (req, res) => {
     try {
         res
-        .setHeader("Content-Type", "text/plain; charset=UTF-8")
-        .setHeader("Access-Control-Allow-Origin", "*")
+        .set(setContentTypeText)
+        .set(CORS)
         .end(func);
     } catch (error) {
         console.error("Произошла ошибка:", error);
         res.status(500).send("Произошла ошибка");
     }
-});
-
-app.listen(3000, () => {
+})
+//Task3
+.get("/promise/", (req, res) => {
+    const taskCode = "function task(x) {\nreturn new Promise ( (res, rej) => {\nx < 18 ? res('yes') : rej('no')\n});\n}";
+    res
+    .set(setContentTypeText)
+    .send(taskCode);
+})
+.get("/fetch/", (req, res) => {
+    const __filename = fileURLToPath(import.meta.url).replace("index.js", "task3.html");
+    res
+    .set(setContentTypeHTML)
+    .sendFile(__filename);
+})
+.listen(3000, () => {
     console.log('Сервер запущен на порту 3000');
 });
+
+function task(x) {
+    return new Promise ( (res, rej) => {
+        x < 18 ? res("yes") : rej("no")
+    });
+};
